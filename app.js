78ed2dd1f1c51f64954a067436b9958883fd72d1
henry22 <henry811010@gmail.com>
2020-04-04 15:13:29 +0800

    Add login using passport and express-session

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 
 const port = 3000
 
@@ -17,6 +19,20 @@ app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'your secret key',
+  resave: 'false',
+  saveUninitialized: 'false'
+}))
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 // 設定路由
 // 首頁
--- routes/user.js
+++ routes/user.js
@@ -13,8 +13,11 @@ router.get('/login', (req, res) => {
 })
 
 // 登入檢查
-router.post('/login', (req, res) => {
-  res.send('login')
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  })(req, res, next)
 })
 
 // 註冊頁面
