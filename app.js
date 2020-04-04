60e73e64870a58077eb6d9de991ac99958ee0db0
henry22 <henry811010@gmail.com>
2020-04-04 14:31:33 +0800

    Add home and user routers

--- app.js
+++ app.js
@@ -24,35 +24,8 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+// 使用路由器
+app.use('/users', require('./routes/user'))
 
 // 設定 express port 3000
 app.listen(port, () => {
---
+++ routes/user.js
@@ -0,0 +1,60 @@
+// routes/user.js
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+// 載入 user model
+const db = require('../models')
+const User = db.User
+
+// 認證系統的路由
+// 登入頁面
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+// 登入檢查
+router.post('/login', (req, res) => {
+  res.send('login')
+})
+
+// 註冊頁面
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+// 註冊檢查
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body
+  User.findOne({ where: { email: email } }).then(user => {
+    if (user) {
+      console.log('user already exists')
+      res.render('register', {
+        name,
+        email,
+        password,
+        password2
+      })
+    } else {
+      //  如果 email 不存在就直接新增
+      const newUser = new User({
+        name,
+        email,
+        password
+      })
+
+      newUser.save()
+        .then(user => {
+          // 新增完成導回首頁
+          res.redirect('/')
+        })
+        .catch(err => console.log(err))
+    }
+  })
+})
+
+// 登出
+router.get('/logout', (req, res) => {
+  res.send('logout')
+})
+
+module.exports = router
